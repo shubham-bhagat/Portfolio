1b746c8f9f733f612c3f512841c7c928121ad91a
shubham-bhagat <38393906+shubham-bhagat@users.noreply.github.com>
2020-08-31 00:54:42 +0530

    Updates

---
+++ precache-manifest.16e6e60a4b90ba4e6aaf5c785fcd9872.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fa6ae6c4b2a4ce38b2d7e131156b6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7360d32ad270e77bd0e1",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f107d20399bf00410f8c",
+    "url": "/portfolio/static/css/main.66954c68.chunk.css"
+  },
+  {
+    "revision": "7360d32ad270e77bd0e1",
+    "url": "/portfolio/static/js/2.a1e75544.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.a1e75544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f107d20399bf00410f8c",
+    "url": "/portfolio/static/js/main.6e13cad9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d0060dbb3d08e91737a5cdecb649955c",
+    "url": "/portfolio/static/media/ShubhamBhagat_CV.d0060dbb.pdf"
+  },
+  {
+    "revision": "f01eb3edd5b0dcf63f1f003376b3c4c9",
+    "url": "/portfolio/static/media/about.f01eb3ed.jpg"
+  },
+  {
+    "revision": "40b009d4c17ab4647ff236489bf1cf7a",
+    "url": "/portfolio/static/media/bike.40b009d4.png"
+  },
+  {
+    "revision": "5a09a09dd1a81f8e3f1cf46d0b40c7a5",
+    "url": "/portfolio/static/media/chart.5a09a09d.svg"
+  },
+  {
+    "revision": "ff4add225792356a07073bccda0ccb76",
+    "url": "/portfolio/static/media/dataImg.ff4add22.png"
+  },
+  {
+    "revision": "17df8328479d8fa262fd73fb71bac1f9",
+    "url": "/portfolio/static/media/ecomImg.17df8328.png"
+  },
+  {
+    "revision": "fe555a68045b385dbbb0798cabf7e61d",
+    "url": "/portfolio/static/media/email.fe555a68.svg"
+  },
+  {
+    "revision": "4c680c66e71f694de3fa868fcf63bb2c",
+    "url": "/portfolio/static/media/git.4c680c66.svg"
+  },
+  {
+    "revision": "c588630ef58d35787121854fb682b585",
+    "url": "/portfolio/static/media/github.c588630e.svg"
+  },
+  {
+    "revision": "4356bdead2aab584279581b6b3af9923",
+    "url": "/portfolio/static/media/linkedin.4356bdea.svg"
+  },
+  {
+    "revision": "e7ccc098fdee284ae94ea7acda096f3a",
+    "url": "/portfolio/static/media/linkf.e7ccc098.svg"
+  },
+  {
+    "revision": "37ed397d38a8fd685d5b3d73e6c82d58",
+    "url": "/portfolio/static/media/profile.37ed397d.jpg"
+  },
+  {
+    "revision": "24c4e7b4c79e6011aff61e9b3ec59d37",
+    "url": "/portfolio/static/media/project.24c4e7b4.png"
+  },
+  {
+    "revision": "2417e554484689e1b69af224eee62784",
+    "url": "/portfolio/static/media/sing.2417e554.jpg"
+  },
+  {
+    "revision": "e8dd52b7c0769887e409b43ca6192b6a",
+    "url": "/portfolio/static/media/tourImg.e8dd52b7.png"
+  }
+]);
